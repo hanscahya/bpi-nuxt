8ebe3b3711bb7451e5e17708485f4e4c84565aa8
Hans Cahya <hanscahya@users.noreply.github.com>
2021-12-13 10:52:10 +0700

    Add lodash package

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/vue-awesome-swiper.js', ssr: false }],
+  plugins: [
+    { src: '~/plugins/vue-awesome-swiper.js', ssr: false },
+    { src: '~/plugins/lodash.js' },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
