6f295dbf1bdde7ee35f29e00030e204e321445e4
Hans Cahya <hanscahya@users.noreply.github.com>
2021-12-13 09:51:45 +0700

    Insert about us page

---
+++ pages/about-us.vue
@@ -0,0 +1,119 @@
+<template lang="pug">
+  div.flex.flex-col
+    OneColumnContent(
+      :title="content.section1.title"
+      :paragraph="content.section1.paragraph"
+      :image="content.section1.image"
+      :bg-image="content.section2.bgImage"
+      :button-link="content.section2.buttonLink"
+      :button-text="content.section2.buttonText"
+    )
+
+    div.py-10.container.self-center(class="sm:px-10 md:px-20 lg:px-40")
+      TwoColumnsContent(
+        v-for="(item, itemIndex) in content.section2.items"
+        :key="itemIndex"
+        :layout="item.layout"
+        :title="item.title"
+        :paragraph="item.paragraph"
+        :link="item.link"
+        :image="item.image"
+      )
+
+    LargeProfile.px-20.py-10(
+      :title="content.section3.highlight.title"
+      :paragraph="content.section3.highlight.paragraph"
+      :button-1-text="content.section3.highlight.button1Link"
+      :button-1-link="content.section3.highlight.button1Text"
+      :button-2-text="content.section3.highlight.button2Link"
+      :button-2-link="content.section3.highlight.button2Text"
+      :image="content.section3.highlight.image"
+    )
+
+    ThumbnailProfile.mb-10(
+      :items="content.section3.items.slice(1, 5)"
+    )
+
+</template>
+
+<script>
+import main from '@/mixins/main.js'
+
+import OneColumnContent from '@/components/OneColumnContent.vue'
+import TwoColumnsContent from '@/components/TwoColumnsContent.vue'
+import LargeProfile from '@/components/LargeProfile.vue'
+import ThumbnailProfile from '@/components/ThumbnailProfile.vue'
+
+export default {
+  components: {
+    OneColumnContent,
+    TwoColumnsContent,
+    LargeProfile,
+    ThumbnailProfile,
+  },
+  mixins: [main],
+
+  data() {
+    return {
+      content: {
+        section1: {
+          title: 'Test',
+          paragraph:
+            'Ipsum ut incididunt aliqua officia anim sint magna laborum. Est reprehenderit occaecat aute ad voluptate id do aliqua sit commodo labore cillum aliquip ullamco.',
+          image: '/images/blank-img.png',
+          bgImage: '/images/slider/cs.jpg',
+          buttonLink: '/',
+          buttonText: 'Read More',
+        },
+        section2: {
+          items: [
+            {
+              title: 'Visi',
+              paragraph:
+                'Ipsum ut incididunt aliqua officia anim sint magna laborum. Est reprehenderit occaecat aute ad voluptate id do aliqua sit commodo labore cillum aliquip ullamco.',
+              image:
+                'https://i.picsum.photos/id/506/600/400.jpg?hmac=MC98idG_CqLYLFS3hI8kZamVJi7EZAXksZOuOQeAjTk',
+              layout: 'default',
+              link: '/',
+            },
+            {
+              title: 'Misi',
+              paragraph:
+                'Ipsum ut incididunt aliqua officia anim sint magna laborum. Est reprehenderit occaecat aute ad voluptate id do aliqua sit commodo labore cillum aliquip ullamco.',
+              image:
+                'https://i.picsum.photos/id/440/600/400.jpg?hmac=Kb3rP_-EUk-uAjI0x9dg5-hPWIQBoW1XEwhQF9bXsFs',
+              layout: 'reverse',
+              link: '/',
+            },
+          ],
+        },
+        section3: {
+          highlight: {
+            title: 'Sambutan Direktur Utama',
+            paragraph:
+              'Excepteur ea nostrud occaecat in do quis consequat aliquip sit tempor esse. Duis nisi elit id consectetur ullamco quis voluptate incididunt dolor reprehenderit sunt ad. Sint deserunt nulla et exercitation ipsum consequat non reprehenderit adipisicing non.',
+            button1Link: '/',
+            button1Text: 'Baca Selengkapnya',
+            button2Link: '/',
+            button2Text: 'Lihat Profil Direktur',
+            image:
+              'https://im3-img.indosatooredoo.com/dataprod/portalcontent/portal/images/metaimages/637391772578405884.png',
+          },
+          items: [],
+        },
+      },
+    }
+  },
+
+  async mounted() {
+    const response = await this.fetchJSON('/json/profile/index.json')
+    this.content.section3.items = response
+
+    setTimeout(() => {
+      this.$store.commit('setLoading', false)
+    }, 200)
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
